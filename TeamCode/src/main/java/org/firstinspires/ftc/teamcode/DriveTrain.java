9034b1865c348ba4a8a51e52f56729363781b4fb
Phillip Price <22138404+PhillipPrice73@users.noreply.github.com>
2024-09-19 18:33:52 -0500

    Add files via upload
    Initial commit for basic teleop functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,72 @@
+// This is the drive train subsystem file.
+// All drive train stuff should be found here.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/** @noinspection ALL*/
+public class DriveTrain {
+
+    DcMotor leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive;
+
+    // All subsystems should have a hardware function that labels all of the hardware required of it.
+    public DriveTrain(HardwareMap hwMap) {
+
+        // Initializes motor names:
+        leftFrontDrive = hwMap.get(DcMotor.class, "leftFront");
+        leftBackDrive = hwMap.get(DcMotor.class, "leftBack");
+        rightFrontDrive = hwMap.get(DcMotor.class, "rightFront");
+        rightBackDrive = hwMap.get(DcMotor.class, "rightBack");
+
+        // Initializes motor directions:
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    // This function needs an axial, lateral, and yaw input. It uses this input to drive the drive train motors.
+    // The last two variables are for direction switching.
+    public void drive(double axial, double lateral, double yaw) {
+
+        double leftFrontPower = 0;
+        double rightFrontPower = 0;
+        double leftBackPower = 0;
+        double rightBackPower = 0;
+
+        leftFrontPower = axial + lateral + yaw;
+        rightFrontPower = axial - lateral - yaw;
+        leftBackPower = axial - lateral + yaw;
+        rightBackPower = axial + lateral - yaw;
+
+        double max;
+
+        // All code below this comment normalizes the values so no wheel power exceeds 100%.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max; // leftFrontPower = leftFrontPower / max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // The next eleven lines gives the calculated power to each motor.
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+    }
+  
+    public void stop() { // Makes the robot stop whenever this function is called
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+}
