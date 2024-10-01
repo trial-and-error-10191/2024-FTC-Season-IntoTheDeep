66fe366af894a295e80fb9405b3158f3f3aeb763
kahvi <kahvidsachi@gmail.com>
2024-10-01 18:41:07 -0500

    Just some reversing of some motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -137,22 +137,35 @@ public class DriveTrain {
         }
     }
     public void autonomous1() { // Autonomous for IntoTheDeep
-        strafeRight(0.7,0.8);
+        strafeRight(0.4, 0.3);
         stop();
         Wait(1);
-        moveVertically(0.5, 0.5);
+        moveVertically(0.9, 1.35);
         stop();
-        Wait(1);
-        moveVertically(1, 0.5);
+        Wait(0.1);
+        moveVertically(-0.1, 0.1);
         stop();
-        Wait(20);
-        turnCounterClockwise(2, 0.5);
+        Wait(0.3);
+        moveVertically(-0.9, 1.5);
         stop();
-        moveVertically(7, 0.5);
+        Wait(0.1);
+        moveVertically(0.1, 0.1);
         stop();
-        strafeLeft(4, 0.5);
-        stop();
-        moveVertically(-4, 0.5);
-        stop();
+        Wait(0.3);
+//        strafeLeft(0.8, 0.5);
+//        moveVertically(0.5, 0.5);
+//        stop();
+//        Wait(1);
+//        moveVertically(1, 0.5);
+//        stop();
+//        Wait(20);
+//        turnCounterClockwise(2, 0.5);
+//        stop();
+//        moveVertically(7, 0.5);
+//        stop();
+//        strafeLeft(4, 0.5);
+//        stop();
+//        moveVertically(-4, 0.5);
+//        stop();
     }
 }
