cdd0063516655074eeda8dad012f49cf855cc056
kahvi <kahvidsachi@gmail.com>
2024-10-26 15:32:01 -0500

    Doesn't move the robot correctly but we don't know what to do about it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -7,11 +7,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Robot {
     public DriveTrain driveTrain;
+    public AscentMechanism ascentMechanism;
     public Telemetry telemetry;
     // This combines all the subsystems.
     // Nonsense change to test 'git push' through Android Studio
     public Robot(HardwareMap hwMap, Telemetry telemetry) {
         driveTrain = new DriveTrain(hwMap, telemetry);
+        ascentMechanism = new AscentMechanism();
         this.telemetry = telemetry;
     }
 }
