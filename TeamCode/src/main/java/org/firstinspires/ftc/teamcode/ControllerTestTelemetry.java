4e77a323967954d16c3fb2a8ddb3d21dec04363c
kahvi <kahvidsachi@gmail.com>
2024-11-05 13:40:08 -0600

    servo arm to touch bar in autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTestTelemetry.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name = "telemetry test", group = "LinearOpMode")
+public class ControllerTestTelemetry {
+    public void testTeleOp() {
+        while (linearOpMode.opModeIsActive()){
+            telemetry.addData("Status", "Waiting for Start");
+            telemetry.update();
+
+            // Provides telemetry for all motors, servos, and sensors.
+            telemetry.addData("Left Stick X/Y", "%4.2f, %4.2f",
+                    gamepad1.left_stick_x, gamepad1.left_stick_y);
+            telemetry.addData("Right Stick X/Y", "%4.2f, %4.2f",
+                    gamepad1.right_stick_x, gamepad1.right_stick_y);
+            telemetry.addData("Right Trigger", "%4.2f",
+                    gamepad1.right_trigger);
+            telemetry.addData("Left Trigger", "%4.2f",
+                    gamepad1.left_trigger);
+        }
+    }
+}
