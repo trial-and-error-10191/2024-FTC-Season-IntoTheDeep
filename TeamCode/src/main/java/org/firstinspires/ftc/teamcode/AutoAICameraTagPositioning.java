a60c02dce5d0995e0e789ed045228a8e0add40a6
Emily M <emm021810@gmail.com>
2024-12-26 19:42:54 -0600

    Husky Lens (Task from Phil)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAICameraTagPositioning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAICameraTagPositioning.java
@@ -4,9 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 @Autonomous(name = "AutomaticAICameraTagPositioning", group = "Concept")
 //@Disabled
@@ -16,8 +17,8 @@ public class AutoAICameraTagPositioning extends LinearOpMode {
 
     private AprilTagProcessor aprilTag;
     private AprilTagDetection desiredTag = null;
-    int horizontalLine = 300;
-    int verticalLine = 300;
+    int horizontalLine = 310;
+    int verticalLine = 230;
 
     final double SPEED_GAIN  =  0.1; //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
     final double STRAFE_GAIN =  0.1; //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
@@ -37,6 +38,32 @@ public class AutoAICameraTagPositioning extends LinearOpMode {
 
     } // end runOpMode()
 
+private void identifyTagLocation() {
+    List<Recognition> currentRecognitions = tfod.getRecognitions();
+    for (Recognition recognition : currentRecognitions) {
+        double xValue = (recognition.getLeft() + recognition.getRight()) / 2;
+        double yValue = (recognition.getRight() + recognition.getLeft()) / 2;
+        // To figure out this part, you will have to use the ConceptTensorFlowObjectDetection file
+        // The first two x values represent the minimum and maximum value x has to be for the team prop to be considered center.
+        // The second two y values represent the minimum and maximum value x has to be for the team prop to be considered center.
+        if (xValue < horizontalLine) {
 
+        }
+
+        if (yValue < verticalLine) {
+
+        }
+
+        if (xValue > horizontalLine) {
+
+        }
+
+        if (yValue > verticalLine) {
+
+        }
+
+    }
+
+}
 
 } // end of class
