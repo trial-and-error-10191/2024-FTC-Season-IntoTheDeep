9c4a275cd7353f76e2fdfc80acf66ca871079b16
Phillip Price <trafub@gmail.com>
2025-01-07 13:12:46 -0600

    Allow both drivers to control every aspect of the robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedResponsibilityGamepad.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class CombinedResponsibilityGamepad extends Gamepad {
+    CombinedResponsibilityGamepad() {}
+
+
+    public void MergeGamepads(Gamepad player1, Gamepad player2) {
+        if (player1 != null && player2 != null) {
+            // Controls from player 1
+            this.left_stick_y = (player1.left_stick_y + player2.left_stick_x) / 2.0f;
+            this.left_stick_x = (player1.left_stick_x + player2.left_stick_x) / 2.0f;
+            this.left_stick_button = player1.left_stick_button || player2.left_stick_button;
+            this.right_stick_y = (player1.right_stick_y + player2.right_stick_y) / 2.0f;
+            this.right_stick_x = (player1.right_stick_x + player2.right_stick_x) / 2.0f;
+            this.right_stick_button = player1.right_stick_button || player2.right_stick_button;
+
+            this.left_bumper = player1.left_bumper || player2.left_bumper;
+            this.right_bumper = player1.right_bumper || player2.right_bumper;
+            this.left_trigger = (player1.left_trigger + player2.left_trigger) / 2.0f;
+            this.right_trigger = (player1.right_trigger + player2.right_trigger) / 2.0f;
+
+            // Controls from player 2
+            this.a = player1.a || player2.a;
+            this.b = player1.b || player2.b;
+            this.x = player1.x || player2.x;
+            this.y = player1.y || player2.y;
+
+            this.dpad_down = player1.dpad_down || player2.dpad_down;
+            this.dpad_left = player1.dpad_left || player2.dpad_left;
+            this.dpad_up = player1.dpad_up || player2.dpad_up;
+            this.dpad_right = player1.dpad_right || player2.dpad_right;
+        } else if (player1 == null && player2 == null) {
+            SetDefaultValues();
+        }else if (player1 == null) {
+            CopyGamepad(player2);
+        } else { // player2 == null
+            CopyGamepad(player1);
+        }
+    }
+
+
+    private void CopyGamepad(Gamepad gamepad) {
+        this.a = gamepad.a;
+        this.b = gamepad.b;
+        this.x = gamepad.x;
+        this.y = gamepad.y;
+
+        this.dpad_right = gamepad.dpad_right;
+        this.dpad_up = gamepad.dpad_up;
+        this.dpad_left = gamepad.dpad_left;
+        this.dpad_down = gamepad.dpad_down;
+
+        this.left_bumper = gamepad.left_bumper;
+        this.right_bumper = gamepad.right_bumper;
+        this.left_trigger = gamepad.left_trigger;
+        this.right_trigger = gamepad.right_trigger;
+
+        this.left_stick_button = gamepad.left_stick_button;
+        this.left_stick_x = gamepad.left_stick_x;
+        this.left_stick_y = gamepad.left_stick_y;
+        this.right_stick_button = gamepad.right_stick_button;
+        this.right_stick_x = gamepad.right_stick_x;
+        this.right_stick_y = gamepad.right_stick_y;
+    }
+
+
+    private void SetDefaultValues() {
+        this.a = false;
+        this.b = false;
+        this.x = false;
+        this.y = false;
+
+        this.dpad_right = false;
+        this.dpad_up = false;
+        this.dpad_left = false;
+        this.dpad_down = false;
+
+        this.left_bumper = false;
+        this.right_bumper = false;
+        this.left_trigger = 0.0f;
+        this.right_trigger = 0.0f;
+
+        this.left_stick_button = false;
+        this.left_stick_x = 0.0f;
+        this.left_stick_y = 0.0f;
+        this.right_stick_button = false;
+        this.right_stick_x = 0.0f;
+        this.right_stick_y = 0.0f;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep_TeleOp.java
@@ -8,14 +8,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "IntoTheDeep TeleOp", group = "LinearOpMode")
 public class IntoTheDeep_TeleOp extends LinearOpMode {
 
+    private boolean UseSplitResponsibilityGamepad = true;
+    private boolean USRGSignalPast = false;
+    private boolean USRGSignalCurrent = false;
     @Override
-
     public void runOpMode() {
 
 
         // Initiates the robots system and subsystems!
         Robot robot = new Robot(hardwareMap, telemetry);
-        SplitResponsibilityGamepad gamepad = new SplitResponsibilityGamepad();
+        SplitResponsibilityGamepad srGamepad = new SplitResponsibilityGamepad();
+        CombinedResponsibilityGamepad crGamepad = new CombinedResponsibilityGamepad();
 
         telemetry.addData("Status", "Waiting for Start");
         telemetry.update();
@@ -24,12 +27,24 @@ public class IntoTheDeep_TeleOp extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Gamepad 1 exists: ", "%b", gamepad1 != null);
             telemetry.addData("Gamepad 2 exists: ", "%b", gamepad2 != null);
-            gamepad.MergeGamepads(gamepad1, gamepad2);
-            // This controls the drive train using three double input methods.
-            // The fourth input is a boolean for the direction toggle.
-            // The last input is the time the function uses to space out inputs for the direction switch.
-            //robot.driveByPower(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-            robot.driveByPower(-gamepad.left_stick_y, gamepad.left_stick_x, gamepad.right_stick_x);
+
+            // Only change on new button press.
+            // If button is pressed, we only want to switch once, not oscillate rapidly back and forth
+            USRGSignalCurrent = gamepad1.back || gamepad1.guide;
+            if (!USRGSignalPast && USRGSignalCurrent) {
+                UseSplitResponsibilityGamepad = !UseSplitResponsibilityGamepad;
+            }
+            USRGSignalPast = USRGSignalCurrent;
+
+            telemetry.addData("Using Split Responsibility Controller: ", "%b", UseSplitResponsibilityGamepad);
+
+            if (UseSplitResponsibilityGamepad) {
+                srGamepad.MergeGamepads(gamepad1, gamepad2);
+                robot.driveByPower(-srGamepad.left_stick_y, srGamepad.left_stick_x, srGamepad.right_stick_x);
+            } else {
+                crGamepad.MergeGamepads(gamepad1, gamepad2);
+                robot.driveByPower(-crGamepad.left_stick_y, crGamepad.left_stick_x, crGamepad.right_stick_x);
+            }
 
             // Provides telemetry for all motors, servos, and sensors.
             telemetry.addData("Front Driving Motors (Left, Right)", "%4.2f, %4.2f",
