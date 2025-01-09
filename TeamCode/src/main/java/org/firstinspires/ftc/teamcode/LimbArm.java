371941ef5cdfb369630c514ef29caa30f44b650a
kahvi <kahvidsachi@gmail.com>
2025-01-08 22:26:43 -0600

    New limb code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
@@ -6,10 +6,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class LimbArm {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
-    static final double MAX_POS     =  1.0;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;     // Minimum rotational position
     DcMotor limbExtend, limbRotate;
-    double position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double power = 0;
 
     public LimbArm(HardwareMap hwMap) {
         limbExtend = hwMap.get(DcMotor.class, "limbExtend");
@@ -17,62 +15,38 @@ public class LimbArm {
     }
     public void armExtend(boolean extend, boolean contract) {
         if (extend) {
-            position += INCREMENT;
-            if (position >= MAX_POS) {
-                position = MAX_POS;
-            }
+            power += INCREMENT;
         }
         else if (contract) {
-            position -= INCREMENT;
-            if (position <= MIN_POS) {
-                position = MIN_POS;
-            }
+            power -= INCREMENT;
         }
-        limbExtend.setPower(position);
+        limbExtend.setPower(power);
     }
     public void armExtendAuto(double limbExtendAuto) {
-        if (position < limbExtendAuto) {
-            position += INCREMENT;
-            if (position >= MAX_POS) {
-                position = MAX_POS;
-            }
+        if (power < limbExtendAuto) {
+            power += INCREMENT;
         }
-        else if (position > limbExtendAuto) {
-            position -= INCREMENT;
-            if (position <= MIN_POS) {
-                position = MIN_POS;
-            }
+        else if (power > limbExtendAuto) {
+            power -= INCREMENT;
         }
-        limbExtend.setPower(position);
+        limbExtend.setPower(power);
     }
     public void armRotate(boolean left, boolean right) {
         if (left) {
-            position += INCREMENT;
-            if (position >= MAX_POS) {
-                position = MAX_POS;
-            }
+            power += INCREMENT;
         }
         else if (right) {
-            position -= INCREMENT;
-            if (position <= MIN_POS) {
-                position = MIN_POS;
-            }
+            power -= INCREMENT;
         }
-        limbRotate.setPower(position);
+        limbRotate.setPower(power);
     }
     public void armRotateAuto(double limbRotateAuto) {
-        if (position < limbRotateAuto) {
-            position += INCREMENT;
-            if (position >= MAX_POS) {
-                position = MAX_POS;
-            }
+        if (power < limbRotateAuto) {
+            power += INCREMENT;
         }
-        else if (position > limbRotateAuto) {
-            position -= INCREMENT;
-            if (position <= MIN_POS) {
-                position = MIN_POS;
-            }
+        else if (power > limbRotateAuto) {
+            power -= INCREMENT;
         }
-        limbRotate.setPower(position);
+        limbRotate.setPower(power);
     }
 }
