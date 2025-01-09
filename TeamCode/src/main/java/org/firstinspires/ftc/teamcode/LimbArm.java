a2af74f7da6aae168f0c8dca34b03abac4a640e6
kahvi <kahvidsachi@gmail.com>
2025-01-09 15:24:30 -0600

    New limb code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
@@ -8,6 +8,8 @@ public class LimbArm {
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     DcMotor limbExtend, limbRotate;
     double power = 0;
+    double MAX_POS = 1;
+    double MIN_POS = 0;
 
     public LimbArm(HardwareMap hwMap) {
         limbExtend = hwMap.get(DcMotor.class, "limbExtend");
@@ -16,36 +18,60 @@ public class LimbArm {
     public void armExtend(boolean extend, boolean contract) {
         if (extend) {
             power += INCREMENT;
+            if (power >= MAX_POS) {
+                power = MAX_POS;
+            }
         }
         else if (contract) {
             power -= INCREMENT;
+            if (power <= MIN_POS) {
+                power = MIN_POS;
+            }
         }
         limbExtend.setPower(power);
     }
     public void armExtendAuto(double limbExtendAuto) {
         if (power < limbExtendAuto) {
             power += INCREMENT;
+            if (power >= MAX_POS) {
+                power = MAX_POS;
+            }
         }
         else if (power > limbExtendAuto) {
             power -= INCREMENT;
+            if (power <= MIN_POS) {
+                power = MIN_POS;
+            }
         }
         limbExtend.setPower(power);
     }
     public void armRotate(boolean left, boolean right) {
         if (left) {
             power += INCREMENT;
+            if (power >= MAX_POS) {
+                power = MAX_POS;
+            }
         }
         else if (right) {
             power -= INCREMENT;
+            if (power <= MIN_POS) {
+                power = MIN_POS;
+            }
         }
         limbRotate.setPower(power);
     }
     public void armRotateAuto(double limbRotateAuto) {
         if (power < limbRotateAuto) {
             power += INCREMENT;
+            if (power >= MAX_POS) {
+                power = MAX_POS;
+            }
         }
         else if (power > limbRotateAuto) {
             power -= INCREMENT;
+            if (power <= MIN_POS) {
+                power = MIN_POS;
+            }
         }
         limbRotate.setPower(power);
     }
