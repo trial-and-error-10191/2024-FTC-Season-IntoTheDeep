522ecbd9faa98c4408b59b33b5124b743ac310db
Phillip Price <trafub@gmail.com>
2025-01-27 23:59:30 -0600

    Rotate arm by encoder counts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimbArm.java
@@ -10,14 +10,16 @@ public class LimbArm {
     DcMotor limbExtend, limbRotate;             // DC motors for lift arm
     CRServo spoolServo;                         // Servo that hold wires for lift
     private final double EXTEND_POWER = 0.5;                      // Motor power for lift extension
-    double rotatePower = 0;                     // Motor power for lift rotation
+    private final double ROTATE_POWER = 0.5;                     // Motor power for lift rotation
     int extensionLimit = 3780;                     // Limit for extension
     final int maxExtendPos = 3780;             // Encoder counter max for lift extension
-    int maxRotatePos = -2356;                   // Encoder counter for lift rotation
+    int maxRotatePos = -2356;                  // max encoder counter for lift rotation
+    int rotatePos = 0;                         // Encoder counter for lift rotation
     int targetPosition = 0;
     DigitalChannel limitExtend;                 // Limit switch for bottom lift position
     DigitalChannel limitRotate;                 // Limit switch to prevent lift rotation
     private final int EXTENSION_RATE = 160;
+    private final int ROTATION_RATE = 40;
 
     public LimbArm(HardwareMap hwMap) {
         limbExtend = hwMap.get(DcMotor.class, "limbExtend");
@@ -29,6 +31,11 @@ public class LimbArm {
         limbExtend.setPower(EXTEND_POWER);
 
         limbRotate = hwMap.get(DcMotor.class, "limbRotate");
+        limbRotate.setDirection(DcMotor.Direction.FORWARD);
+        rotatePos = limbRotate.getCurrentPosition();
+        limbRotate.setTargetPosition(rotatePos);
+        limbRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        limbRotate.setPower(ROTATE_POWER);
 
         spoolServo = hwMap.get(CRServo.class, "spoolServo");
 
@@ -77,23 +84,15 @@ public class LimbArm {
             targetPosition = extensionLimit;
             limbExtend.setTargetPosition(targetPosition);
         }
-        if (turn < 0) {                           // Makes the arm rotate down?
-            rotatePower = turn;
-            if (limbRotate.getCurrentPosition() <= maxRotatePos) {
-                rotatePower = 0;
-            }
+        rotatePos = limbRotate.getCurrentPosition() + (int)(turn * ROTATION_RATE);
+        if (rotatePos < maxRotatePos) {
+            rotatePos = maxRotatePos;
+        } else if (turn > 0 && !limitRotate.getState()) {
+            limbRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rotatePos = limbRotate.getCurrentPosition();
+            limbRotate.setTargetPosition(rotatePos);
+            limbRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
-        else if (turn > 0) {                      // Makes the arm rotate up?
-            rotatePower = turn;
-            if (!limitRotate.getState()) {      // Stop motor and reset encoder if limit switch is triggered
-                limbRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                limbRotate.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                rotatePower = 0;
-            }
-        }
-        else {
-            rotatePower = 0;
-        }
-        limbRotate.setPower(rotatePower);
+        limbRotate.setTargetPosition(rotatePos);
     }
 }
