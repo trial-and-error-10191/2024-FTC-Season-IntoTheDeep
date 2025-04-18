86221ebb306a0fbb7635dfdc3ca3718be590a45e
kahvi <kahvidsachi@gmail.com>
2025-04-17 20:51:59 -0500

    Post-merge fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Robot.java
@@ -1,9 +1,13 @@
 // This file is a system file.
 package org.firstinspires.ftc.teamcode.Assemblies;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Autonomous.AdvancedAutoBasketSpike;
 
 public class Robot {
 
@@ -53,8 +57,11 @@ public class Robot {
             limbArm.maxRotatePos = -2356;
         }
         else if (state == ROBOT_HUNTER.SAMPLE_HUNT) {
-            if (limbArm.limbRotate.getCurrentPosition() < -1000 && limbArm.limbRotate.getCurrentPosition() >= 0) {
+            if (limbArm.limbRotate.getCurrentPosition() < -1000 || limbArm.limbRotate.getCurrentPosition() >= 0) {
                 limbArm.limbRotate.setTargetPosition(-988);
+                Wait(2);
+                limbArm.limbRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                limbArm.limbRotate.setPower(limbArm.ROTATE_POWER);
             }
             limbArm.move(gamepad2);
             driveTrain.turnToHeading(DriveTrain.TURN_SPEED, -90);
@@ -68,5 +75,13 @@ public class Robot {
             return true;
         }
         return false;
+    }
+
+    public void Wait(double seconds) {
+        ElapsedTime Time   = new ElapsedTime();
+        Time.reset();
+        while (Time.milliseconds()  < seconds * 1000) {
+            // doesn't need anything
+        } // end of while loop
     }
 }
