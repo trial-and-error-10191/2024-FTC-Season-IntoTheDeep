3a094c3a11324bd12b464157b8e89388f0374c0c
kahvi <kahvidsachi@gmail.com>
2025-06-10 19:06:47 -0500

    Getting Auto paths to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Field_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Field_TeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Teleoperation;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
