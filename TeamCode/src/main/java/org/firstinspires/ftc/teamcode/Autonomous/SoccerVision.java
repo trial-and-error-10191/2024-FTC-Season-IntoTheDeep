99cafa25dfbc8a4385bb70975f2312ced12d6b9b
kahvi <kahvidsachi@gmail.com>
2025-08-14 20:33:46 -0500

    Working on getting the robot to detect a ball

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SoccerVision.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVisionColorSensor;
+
+@Autonomous(name="SoccerVision", group="Robot")
+public class SoccerVision extends ConceptVisionColorSensor {
+
+}
