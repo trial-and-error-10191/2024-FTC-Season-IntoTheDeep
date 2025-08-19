029415783ae2f27d2b20f47f2bfd2d5922dcfeb0
kahvi <kahvidsachi@gmail.com>
2025-08-19 16:00:24 -0500

    Basically copying a file into another one for ball detecting purposes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SoccerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SoccerVision.java
@@ -1,10 +1,90 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import android.graphics.Color;
+import android.util.Size;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVisionColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor;
 
 @Autonomous(name="SoccerVision", group="Robot")
-public class SoccerVision extends ConceptVisionColorSensor {
+public class SoccerVision extends LinearOpMode {
+    @Override
+    public void runOpMode()
+    {
+        /* Build a "Color Sensor" vision processor based on the PredominantColorProcessor class.
+         *
+         * - Focus the color sensor by defining a RegionOfInterest (ROI) which you want to inspect.
+         *     This can be the entire frame, or a sub-region defined using:
+         *     1) standard image coordinates or 2) a normalized +/- 1.0 coordinate system.
+         *     Use one form of the ImageRegion class to define the ROI.
+         *         ImageRegion.entireFrame()
+         *         ImageRegion.asImageCoordinates(50, 50,  150, 150)  100x100 pixel square near the upper left corner
+         *         ImageRegion.asUnityCenterCoordinates(-0.1, 0.1, 0.1, -0.1)  10% width/height square centered on screen
+         *
+         * - Set the list of "acceptable" color swatches (matches).
+         *     Only colors that you assign here will be returned.
+         *     If you know the sensor will be pointing to one of a few specific colors, enter them here.
+         *     Or, if the sensor may be pointed randomly, provide some additional colors that may match the surrounding.
+         *     Possible choices are:
+         *         RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, MAGENTA, BLACK, WHITE;
+         *
+         *     Note that in the example shown below, only some of the available colors are included.
+         *     This will force any other colored region into one of these colors.
+         *     eg: Green may be reported as YELLOW, as this may be the "closest" match.
+         */
+        PredominantColorProcessor colorSensor = new PredominantColorProcessor.Builder()
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-0.1, 0.1, 0.1, -0.1))
+                .setSwatches(
+                        PredominantColorProcessor.Swatch.RED,
+                        PredominantColorProcessor.Swatch.BLUE,
+                        PredominantColorProcessor.Swatch.YELLOW,
+                        PredominantColorProcessor.Swatch.BLACK,
+                        PredominantColorProcessor.Swatch.WHITE)
+                .build();
 
+        /*
+         * Build a vision portal to run the Color Sensor process.
+         *
+         *  - Add the colorSensor process created above.
+         *  - Set the desired video resolution.
+         *      Since a high resolution will not improve this process, choose a lower resolution that is
+         *      supported by your camera.  This will improve overall performance and reduce latency.
+         *  - Choose your video source.  This may be
+         *      .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))  .....   for a webcam
+         *  or
+         *      .setCamera(BuiltinCameraDirection.BACK)    ... for a Phone Camera
+         */
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessor(colorSensor)
+                .setCameraResolution(new Size(320, 240))
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+
+        telemetry.setMsTransmissionInterval(50);  // Speed up telemetry updates, Just use for debugging.
+
+        // WARNING:  To be able to view the stream preview on the Driver Station, this code runs in INIT mode.
+        while (opModeIsActive() || opModeInInit())
+        {
+            telemetry.addData("DS preview on/off", "3 dots, Camera Stream\n");
+
+            // Request the most recent color analysis.
+            // This will return the closest matching colorSwatch and the predominant RGB color.
+            // Note: to take actions based on the detected color, simply use the colorSwatch in a comparison or switch.
+            //  eg:
+            //      if (result.closestSwatch == PredominantColorProcessor.Swatch.RED) {... some code  ...}
+            PredominantColorProcessor.Result result = colorSensor.getAnalysis();
+
+            // Display the Color Sensor result.
+            telemetry.addData("Best Match:", result.closestSwatch);
+            telemetry.addLine(String.format("R %3d, G %3d, B %3d", Color.red(result.rgb), Color.green(result.rgb), Color.blue(result.rgb)));
+            telemetry.update();
+
+            sleep(20);
+        }
+    }
 }
